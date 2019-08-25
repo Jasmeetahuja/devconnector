dfed2ab3deb2fbfed7ba9cc5903052a2aa40dc77
JasmeetAhuja <jassideep013@gmail.com>
2019-08-25 17:36:29 +0530

    auth and login/logout

--- jassi/src/components/auth/Register.js
+++ jassi/src/components/auth/Register.js
@@ -1,10 +1,11 @@
-import React, { Fragment, useState } from 'react';
+import React,{Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { setAlert } from  '../../actions/alert';
+import { Link, Redirect } from 'react-router-dom'; 
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
    const [formData, setFormData] = useState({
        name: '',
        email: '',
@@ -19,7 +20,7 @@ const Register = ({ setAlert }) => {
    const onSubmit = async e => {
        e.preventDefault();
        if(password !== password2) {
-          setAlert('Passwords do not match', 'danger');
+          setAlert('Passwords do not match!', 'danger');
        } else {
     //        const newUser = {
     //            name,
@@ -42,9 +43,14 @@ const Register = ({ setAlert }) => {
     //            console.error(err.response.data);
     //        }
     //    }
-    console.log('SUCCESS');
-   };
+    register({ name, email, password });
+   }
 };
+
+   //Redirect if logged in 
+   if(isAuthenticated){
+    return <Redirect to='/dashboard' />;
+  }
  
     return (
         <Fragment>
@@ -103,7 +109,13 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
-export default connect( null, { setAlert })(Register);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps,{setAlert,register})(Register);
